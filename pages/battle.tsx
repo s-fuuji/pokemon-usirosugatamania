7c716c0ae19898a9fcfa525cd4738e53c34e1570
ofuji <soutoumasunaga@gmail.com>
2022-06-15 20:12:05 +0900

    使いまわせる乱数作成コンポーネントを作成

--- pages/battle.tsx
+++ pages/battle.tsx
@@ -2,16 +2,17 @@ import { Button, Image } from "@mantine/core";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { isMemberName } from "typescript";
+import useSWR from "swr";
 import { usePokeSWR } from "../hooks/usePokeSwr";
 import { storeState } from "../slicer/store";
 
 const Battle: NextPage = () => {
-    const [rivalPoke, setRivalPoke] = useState([])
-    const [isCheckDisabled, setIsCheckDisabled] = useState(false)
+    const [rivalParty, setRivalParty] = useState<number[]>([])
     const [isDisabled, setIsDisabled] = useState(false)
     const got = useSelector((state: storeState) => state.got)
-    const [myBattleParty, setMyBattleParty]: any[] = useState(
+    const { pokemonList, pokemonListError } = usePokeSWR();
+
+    const [myBattleParty, setMyBattleParty] = useState<any[]>(
         got.map((got: any) => {
             return ({
                 pokeIndex: got,
@@ -19,17 +20,38 @@ const Battle: NextPage = () => {
             })
         })
     )
-    const { pokemonList, pokemonListError } = usePokeSWR();
+    const [playerStatus, setPlayerStatus] = useState([
+        { hp: 50, timeLiset: 3 },
+        { hp: 50, timeLiset: 3 }
+    ])
+    const [partyStatus, setPartyStatus] = useState()
 
+
+    const randomNumber = (min: number, max: number) => {
+        const createRandoms = (min: number, max: number) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        const randoms: any = [];
+        [...Array(max)].map(() => {
+            let test = createRandoms(min, max);
+            while (randoms.includes(test)) {
+                test = createRandoms(min, max)
+            }
+            randoms.push(test)
+        })
+        return randoms
+    }
 
 
 
 
     const pokemonBattle = () => {
-        const rivalPartyArray = [...Array(6)].map(() => {
+        const newRivalParty = [...Array(6)].map(() => {
             const pickRivalPokemon = Math.floor(Math.random() * 40)
             return (pokemonList[pickRivalPokemon])
-        })
+        });
+        setRivalParty(newRivalParty)
+        console.log(rivalParty);
 
     }
 
@@ -46,12 +68,37 @@ const Battle: NextPage = () => {
         )
         setMyBattleParty(toggleParty)
     }
+    /*ここまでが自分の手持ちの選択をする処理 */
+
+    const battleStart = () => {
+        const checkedMyPokemon = myBattleParty.filter(
+            member => member.checked
+        )
 
 
+        /* const newPartyStatus = {
+             "player": checkedMyPokemon.map((member) => { return { "id": pokemonList[member]?.sprites.back_default, "power": 10 } }),
+             "rival": [{ id: index, power:}]
+ 
+         }
+ 
+         setPartyStatus */
+
+    }
 
 
     return (
         <div className="text-center">
+
+            <div className="flex justify-center">
+                {rivalParty?.map((rivalPokeIndex: any) => {
+                    console.log(rivalPokeIndex);
+
+                    return <Image key={Math.random()} src={rivalPokeIndex.sprites.back_default} className="rounded-full w-10" />
+                })}
+            </div>
+            <p className="text-red-500 text-4xl">VS</p>
+
             <div className="flex justify-center">
                 {pokemonList ? got?.map((gotPokeIndex: number, index: number) => {
                     return <label htmlFor={`id_${index}`} key={`key_${index}`} className="flex-col">
@@ -69,15 +116,22 @@ const Battle: NextPage = () => {
             </div>
 
 
-
-            <Button
-                onClick={pokemonBattle}
-                variant="gradient"
-                gradient={{ from: "orange", to: "red" }}
-                style={{ marginBottom: 30 }}
-            >
-                バトルスタート
-            </Button>
+            {!isDisabled ?
+                <Button
+                    onClick={pokemonBattle}
+                    variant="gradient"
+                    gradient={{ from: "orange", to: "red" }}
+                    style={{ marginBottom: 30 }}
+                >
+                    バトルの準備
+                </Button> : <Button
+                    onClick={battleStart}
+                    variant="gradient"
+                    gradient={{ from: "orange", to: "red" }}
+                    style={{ marginBottom: 30 }}
+                >
+                    バトル開始
+                </Button>}
         </div>
     )
 }
