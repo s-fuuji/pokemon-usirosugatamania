ce8d7a2317071693874303782c3309f07be2be18
ofuji <soutoumasunaga@gmail.com>
2022-07-27 10:47:20 +0900

    テスト

--- src/components/page/PokeDetail/PokeDetail/index.tsx
+++ src/components/page/PokeDetail/PokeDetail/index.tsx
@@ -1,13 +1,11 @@
 import { PokeCard } from '../../../model/Top/ui/PokeCard'
 import { useRouter } from 'next/router'
-import { GetStaticProps, NextPage } from 'next/types'
-import { GetServerSideProps } from 'next/types'
+import { NextPage } from 'next/types'
 import React from 'react'
-import { fetcherAll, usePokeSWR } from '../../../../api/usePokeSwr'
-import { SWRConfig } from 'swr'
+import { usePokeSWR } from '../../../../api/usePokeSwr'
 
 export const PokeDetailPage: NextPage = () => {
-  const { pokemonList, error, isLoading } = usePokeSWR()
+  const { pokemonList, isLoading } = usePokeSWR()
   const id = Number(useRouter().query.id) - 1
   const pokemonListId = pokemonList ? pokemonList[id] : []
   const indexId = Number(id) + 1
@@ -17,18 +15,23 @@ export const PokeDetailPage: NextPage = () => {
   }
 
   return (
-    <div className="flex justify-center">
-      <PokeCard
-        imgUrl={pokemonListId.sprites.back_default}
-        link="/"
-        index={indexId}
-      />
-      {pokemonListId.sprites.back_female && (
+    <div className="flex justify-between">
+      <div>
         <PokeCard
-          imgUrl={pokemonListId.sprites.back_female}
+          imgUrl={pokemonListId.sprites.back_default}
           link="/"
           index={indexId}
         />
+      </div>
+
+      {pokemonListId.sprites.back_female && (
+        <div>
+          <PokeCard
+            imgUrl={pokemonListId.sprites.back_female}
+            link="/"
+            index={indexId}
+          />
+        </div>
       )}
     </div>
   )
